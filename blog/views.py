8fd2e2efb99e8ae3834496540629714513f71a3b
DJ Knox <dhknox@gmail.com>
2019-09-07 13:16:09 -0400

    create scaffolding for home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
