09e84bdedab29c2ad0b57dddaf0a398cc89cd86a
DJ Knox <dhknox@gmail.com>
2019-09-07 13:36:35 -0400

    create scaffolding for post detail pages

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from blog.models import Post
 
 def index(request):
     posts = Post.objects.all()
 
-    context = {
+    return render(request, 'index.html', {
         'posts': posts
-    }
-
-    return render(request, 'index.html', context)
+    })
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'post_detail.html', {
+        'post': post
+    })
